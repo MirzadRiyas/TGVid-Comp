6a3158ade9c3dd7f8f40b9afdb28f841ad2584fc
MirzadRiyas <91402777+MirzadRiyas@users.noreply.github.com>
2022-10-19 21:52:14 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,13 +4,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1664850827
-    OWNER = config("OWNER")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    APP_ID = config("APP_ID", "17124006")
+    API_HASH = config("API_HASH", "97e184dc9c07513de148002859aef2b2")
+    BOT_TOKEN = config("BOT_TOKEN", "5301928162:AAEmDl0Au1z8IQkncjDsmojAO7akB8Mj9Qg")
+    DEV = 1165699179
+    OWNER = config("OWNER", "1165699179")
+    ffmpegcode = ["-preset slow -c:v libx264 -s 1280x720 -x264-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By @PokeTide (https://t.me/PokeTide)' -pix_fmt yuv420p -crf 26 -c:a libopus -b:a 64k -c:s copy -map 0 -ac 2 -ab 64k -vbr 2 -level 3.1 -threads 1"]
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/96a8e440ca77fbf5b4c67.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
